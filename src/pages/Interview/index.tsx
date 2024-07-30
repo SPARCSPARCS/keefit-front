5a12a3a84ce060f7d4d640a791317fd872bb5b1d
Dipokal HHJ <hhj@devent.kr>
2024-07-30 14:23:14 +0900

    feat: get face deg

--- src/pages/Interview/index.tsx
+++ src/pages/Interview/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "../../components/Button";
 import { css } from "@emotion/react";
 import { useAudio } from "../../hooks/useAudio";
@@ -12,6 +12,7 @@ import { useNavigate } from "react-router-dom";
 import { Progress } from "../../components/Progress";
 import { DecibelCircle } from "../../components/DecibelCircle";
 import { CreateQuestion } from "./CreateQuestion";
+import * as vision from "@mediapipe/tasks-vision";
 
 export function InterviewPage() {
   const navigate = useNavigate();
@@ -23,11 +24,18 @@ export function InterviewPage() {
   const [activePage, setActivePage] = useState("InputUrl");
   const [nowPageIndex, setNowPageIndex] = useState(0);
   const [nowQuestionIndex, setNowQuestionIndex] = useState(-1);
+  const [webcamRunning, setWebcamRunning] = useState(false);
+  const [faceResult, setFaceResult] = useState<any>();
+  const [faceLandmarker, setFaceLandmarker] = useState<any>();
+  const [faceDeg, setFaceDeg] = useState(0);
 
   const pages = ["InputUrl", "ShowRecruitment", "CreateQuestion", "Record"];
 
   const recruitment = useInterviewStore((state: any) => state.recruitment);
   const questions = useInterviewStore((state: any) => state.questions);
+
+  const lastVideoTime = useRef(0);
+  const videoRef = useRef(null);
 
   const setRecruitment = useInterviewStore(
     (state: any) => state.setRecruitment
@@ -47,6 +55,9 @@ export function InterviewPage() {
   };
 
   const record = async () => {
+    if (nowQuestionIndex == -1) {
+      enableCam();
+    }
     if (!isStart) {
       setNowQuestionIndex((index) => index + 1);
       if (nowQuestionIndex >= 4) {
@@ -60,6 +71,79 @@ export function InterviewPage() {
       await stopRecord(nowQuestionIndex);
     }
   };
+
+  const createFaceLandmarker = async () => {
+    const filesetResolver = await vision.FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
+    );
+    const faceLandmarkerTemp = await vision.FaceLandmarker.createFromOptions(
+      filesetResolver,
+      {
+        baseOptions: {
+          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+          delegate: "GPU",
+        },
+        outputFaceBlendshapes: true,
+        runningMode: "VIDEO",
+        numFaces: 1,
+        minTrackingConfidence: 0.2,
+      }
+    );
+
+    setFaceLandmarker(faceLandmarkerTemp);
+
+    console.log(faceLandmarkerTemp);
+  };
+
+  function enableCam() {
+    if (webcamRunning === true) {
+      setWebcamRunning(false);
+    } else {
+      setWebcamRunning(true);
+    }
+    const constraints = {
+      video: true,
+    };
+
+    const videoElement: any = document.getElementById("inputVideo");
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      videoElement.srcObject = stream;
+      videoElement.play();
+
+      videoElement.addEventListener("loadeddata", predictWebcam);
+    });
+  }
+
+  async function predictWebcam() {
+    window.requestAnimationFrame(predictWebcam);
+
+    try {
+      let startTimeMs = performance.now();
+      let lm;
+
+      const videoElement: any = document.getElementById("inputVideo");
+
+      if (lastVideoTime.current !== videoElement.currentTime) {
+        lastVideoTime.current = videoElement.currentTime;
+        lm = faceLandmarker.detectForVideo(videoElement, startTimeMs);
+      }
+
+      if (lm.faceLandmarks) {
+        for (const landmarks of lm.faceLandmarks) {
+          var rad = Math.atan2(
+            landmarks[454].x - landmarks[234].x,
+            landmarks[454].z - landmarks[234].z
+          );
+          let l = (rad * 180) / Math.PI;
+          setFaceDeg(l - 90);
+        }
+      }
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    createFaceLandmarker();
+  }, []);
 
   return (
     <>
@@ -109,9 +193,30 @@ export function InterviewPage() {
             controls
           ></audio>
 
-          {/* {audioUrl != "" && <Button onClick={upload}>업로드</Button>} */}
+          <div
+            css={css({
+              display: "flex",
+              flexDirection: "column",
+              gap: "1rem",
+              justifyContent: "center",
+              alignItems: "center",
+            })}
+          >
+            <video
+              id="inputVideo"
+              style={{
+                width: "380px",
+                borderRadius: "2rem",
+              }}
+              ref={videoRef}
+            ></video>
 
-          <DecibelCircle decibel={decibel} />
+            <p>{Math.abs(faceDeg) > 10 ? "정면울 바라봐주세요" : ""}</p>
+
+            <DecibelCircle decibel={decibel} />
+          </div>
+
+          {/* {audioUrl != "" && <Button onClick={upload}>업로드</Button>} */}
 
           <div
             style={{
